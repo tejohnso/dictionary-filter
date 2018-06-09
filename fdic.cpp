e9fef04478ab329f808d8b9b1e7db8e5ad448e53
Tyler Johnson <mail@tyler-johnson.ca>
2018-06-09 12:38:20 -0400

    Refactor into functions

--- fdic.cpp
+++ fdic.cpp
@@ -10,26 +10,46 @@
 
 using namespace std;
 
+void populate_dictionary_from_file(set<string>&, const char *);
+void populate_dictionary_from_args(set<string>&, int, char *[]);
+void filter_input(set<string>);
+
 int main(int argc, char *argv[]) {
   set<string> dict;
-  string filePath;
 
   if (argc > 1) {
-    copy(argv, argv + argc, inserter(dict, dict.end()));
+    populate_dictionary_from_args(dict, argc, argv);
   } else {
-    const char *homedir = getenv("HOME");
-    if (homedir == NULL) {
-      homedir = getpwuid(getuid())->pw_dir;
-    }
-
-    fstream file(filePath.append(homedir).append("/.config/fdic/dict.txt"));
-    copy(istream_iterator<string>(file), istream_iterator<string>(), inserter(dict, dict.end()));
-    if (dict.size() == 0) {
-      cout << "No dictionary found in $HOME/.config/fdic/dict.txt and no words provided as command line arguments." << endl;
-      return 1;
-    }
+    populate_dictionary_from_file(dict, getenv("HOME"));
   }
 
+  if (dict.size() == 0) {
+    string err = "No dictionary found in $HOME/.config/fdic/dict.txt "
+                 "and no words provided as command line arguments.";
+
+    cout << err << endl;
+    return 1;
+  }
+
+  filter_input(dict);
+}
+
+void populate_dictionary_from_args(set<string> &dict, int argc, char *argv[]) {
+  copy(argv + 1, argv + argc, inserter(dict, dict.end()));
+}
+
+void populate_dictionary_from_file(set<string> &dict, const char *homedir) {
+  string filePath;
+
+  if (homedir == NULL) {
+    homedir = getpwuid(getuid())->pw_dir;
+  }
+
+  fstream file(filePath.append(homedir).append("/.config/fdic/dict.txt"));
+  copy(istream_iterator<string>(file), istream_iterator<string>(), inserter(dict, dict.end()));
+}
+
+void filter_input(set<string> dict) {
   for (string word; getline(cin, word);) {
     if (dict.find(word) == dict.end()) {continue;}
     cout << word << '\n';
